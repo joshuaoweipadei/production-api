7f9b2145616201b99ef7068299fb1273dcf9da61
joshuaoweipadei <oweipadeijoshie@gmail.com>
2025-09-26 02:12:39 -0400

    Implement Eslint & Prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello Production API');
+  res.status(200).send('Hello Production API');
 });
 
 export default app;
