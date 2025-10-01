854272ad83b379250f96857f338704243e777a9a
joshuaoweipadei <oweipadeijoshie@gmail.com>
2025-10-01 14:17:56 -0400

    Secure app with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
-
+import securityMiddleware from '#middleware/security.middleware.js'
 const app = express();
 
 app.use(helmet());
@@ -19,6 +19,8 @@ app.use(
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello form production api');
---
+++ src/config/arcjet.js
@@ -0,0 +1,24 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "CATEGORY:PREVIEW",
+      ],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: '2s',
+      max: 5,
+    }),
+  ],
+});
+
+export default aj;
